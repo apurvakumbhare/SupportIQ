0fc20f8d6b36323de4b157d395f080a14bd769eb
Apurva Kumbhare <kumbhareav@rknec.edu>
2025-12-05 13:13:22 +0530

    Adding Files

---
+++ src/main/java/com/example/SupportIQ/SupportIqApplication.java
@@ -0,0 +1,13 @@
+package com.example.SupportIQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportIqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportIqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SupportIQ/SupportIqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SupportIQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportIqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
